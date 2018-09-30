364209dc370a4177852c9eab7ab7778ec513b8b3
Andreas Litt <andreas.litt@wlw.de>
2018-09-30 12:45:21 +0200

    Implement basic cli commands

--- examples/simple/stack.js
+++ examples/simple/stack.js
@@ -1,4 +1,4 @@
-const { Stack, dependsOn } = require("terrastack");
+const { Stack } = require("terrastack");
 const Network = require("@terrastack/terraform-aws-vpc");
 
 const backend = require("./config/backend");
@@ -9,11 +9,11 @@ const simpleStack = new Stack("simple", { backend, provider });
 const network = new Network("network", {
   name: "simple-example",
   cidr: "10.0.0.0/16",
-  azs: ["eu-west-1a", "eu-west-1b", "eu-west-1c"],
+  azs: ["eu-central-1a", "eu-central-1b", "eu-central-1c"],
   private_subnets: ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"],
   public_subnets: ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"],
   assign_generated_ipv6_cidr_block: true,
-  enable_nat_gateway: true,
+  enable_nat_gateway: false,
   single_nat_gateway: true,
   public_subnet_tags: {
     Name: "overridden-name-public"
@@ -28,9 +28,4 @@ const network = new Network("network", {
 });
 simpleStack.add(network);
 
-(async () => {
-  await simpleStack.compile();
-  await simpleStack.init();
-  await simpleStack.plan();
-  // await simpleStack.apply();
-})();
+module.exports = simpleStack;
