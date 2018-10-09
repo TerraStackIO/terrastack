d75919791a0f7a10b2b83e15d28b5d71225deac5
Sebastian Korfmann <korfmann.sebastian@gmail.com>
2018-10-09 21:03:53 +0200

    Mozilla Public License Version 2.0 (#6)

--- examples/complex/stack.js
+++ examples/complex/stack.js
@@ -1,3 +1,9 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 const { Stack, Backend, Provider } = require("terrastack");
 const Network = require("@terrastack/terraform-aws-vpc");
 const EC2Instance = require("@terrastack/terraform-aws-ec2-instance");
